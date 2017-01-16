a7d0da1f7580cb05cbd38941848170adf769ce9a
kfinstotheleft <kfinstotheleft@gmail.com>
2017-01-16 08:14:36 -0800

    added drill 47

---
+++ drill47of68.py
@@ -0,0 +1,13 @@
+# Author: Kristen Findley
+# Date: 14 January, 2017
+# Description: Python drill 47 of 68
+
+for i in range(4):
+    print(i)
+
+for i in range(3,-1,-1):
+    print(i)
+
+for i in range(8,0,-2):
+    print(i)
+
