1452182a8f4414e9c82200bfaa39e65848f7f4d4
kfinstotheleft <kfinstotheleft@gmail.com>
2017-01-25 14:24:48 -0800

    added drill 63 of 68

---
+++ drill63of68.py
@@ -0,0 +1,16 @@
+# Author: Kristen Findley
+# Date: 25 January 2017
+# Python: Version 2.7
+# Description: File mover - program to move all .txt files from Folder A to Folder B.
+
+import shutil
+import os
+
+folderA = "/home/kristen/Documents/TechAcademy/Python/FolderA/"
+folderB = "/home/kristen/Documents/TechAcademy/Python/FolderB/"
+
+for i in os.listdir(folderA):
+    if i.endswith(".txt"):
+        shutil.move(folderA + i,folderB)
+
+        
